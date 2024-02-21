d4519ad3325c80340db147853df485f782e12852
dnicholson2017 <dnicholson2017@fau.edu>
2024-02-20 22:48:50 -0500

    conditional rendering logic fully functional for all cases

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -17,7 +17,7 @@ const Events = (props) => {
       });
       setFilterResults(filteredData);
     } else {
-      setFilterResults(Object.keys(props.list.events));
+      setFilterResults([]);
     }
   };
 
@@ -30,40 +30,65 @@ const Events = (props) => {
           onChange={(e) => searchItems(e.target.value)}
         />
       </div>
-      <div className="event-columns">
-        <div>
-          <h3>Date</h3>
-          {filterResults.map((eventId, index) => (
-            <ul key={index}>
-              <li>{props.list.events[eventId].datetime_local}</li>
-            </ul>
-          ))}
+      {(searchInput !== "" && filterResults.length === 0) ? (
+        <div>No results found</div>
+      ) : (
+        <div className="event-columns">
+          {/* Render column headers */}
+          <div>
+            <h3>Date</h3>
+            {filterResults.length === 0 && props.list.events.map((event, index) => (
+              <ul key={index}>
+                <li>{event.datetime_local}</li>
+              </ul>
+            ))}
+            {filterResults.length > 0 && filterResults.map((eventId, index) => (
+              <ul key={index}>
+                <li>{props.list.events[eventId].datetime_local}</li>
+              </ul>
+            ))}
+          </div>
+          <div>
+            <h3>Performer</h3>
+            {filterResults.length === 0 && props.list.events.map((event, index) => (
+              <ul key={index}>
+                <li>{event.performers[0].name}</li>
+              </ul>
+            ))}
+            {filterResults.length > 0 && filterResults.map((eventId, index) => (
+              <ul key={index}>
+                <li>{props.list.events[eventId].performers[0].name}</li>
+              </ul>
+            ))}
+          </div>
+          <div className="min-price">
+            <h3>Min Price</h3>
+            {filterResults.length === 0 && props.list.events.map((event, index) => (
+              <ul key={index}>
+                <li>{event.stats.lowest_price}</li>
+              </ul>
+            ))}
+            {filterResults.length > 0 && filterResults.map((eventId, index) => (
+              <ul key={index}>
+                <li>{props.list.events[eventId].stats.lowest_price}</li>
+              </ul>
+            ))}
+          </div>
+          <div>
+            <h3>Max Price</h3>
+            {filterResults.length === 0 && props.list.events.map((event, index) => (
+              <ul key={index}>
+                <li>{event.stats.highest_price}</li>
+              </ul>
+            ))}
+            {filterResults.length > 0 && filterResults.map((eventId, index) => (
+              <ul key={index}>
+                <li>{props.list.events[eventId].stats.highest_price}</li>
+              </ul>
+            ))}
+          </div>
         </div>
-        <div>
-          <h3>Performer</h3>
-          {filterResults.map((eventId, index) => (
-            <ul key={index}>
-              <li>{props.list.events[eventId].performers[0].name}</li>
-            </ul>
-          ))}
-        </div>
-        <div className="min-price">
-          <h3>Min Price</h3>
-          {filterResults.map((eventId, index) => (
-            <ul key={index}>
-              <li>{props.list.events[eventId].stats.lowest_price}</li>
-            </ul>
-          ))}
-        </div>
-        <div>
-          <h3>Max Price</h3>
-          {filterResults.map((eventId, index) => (
-            <ul key={index}>
-              <li>{props.list.events[eventId].stats.highest_price}</li>
-            </ul>
-          ))}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
